87ccbb4318cd7c52a70566d311fa0d87eb8b1d99
Alaa El-Din Montasser <alaaeldin.montasser@gmail.com>
2012-11-08 21:34:10 +0200

    Added items per user and users' total to order view.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,7 +7,15 @@ class OrdersController < ApplicationController
   def show
     @order = Order.find params[:id]
     @order_items = OrderItem.all conditions: ["order_id = ?", @order.id], :select => 'item_id, user_id, SUM(item_quantity) as item_quantity', :group => 'item_id'
-    @order_total = 0
+    @total_price = 0
+    @order_items.each do |item|
+      @total_price += Item.find(item.item_id).price * item.item_quantity
+    end
+    @user_items = OrderItem.all conditions: ["order_id = ?", @order.id], :select => 'item_id, user_id, SUM(item_quantity) as item_quantity', :group => 'item_id, user_id', order: 'user_id'
+    @username = ""
+    @user_total = 0
+    @user_total_hash = {}
+
   end
 
   def close_order
